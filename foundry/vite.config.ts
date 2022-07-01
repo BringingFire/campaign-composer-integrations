c1b148b20e453ac0a8d24a6842ac5ddf1af8493d
Joshua Matthews <josh@jmatth.com>
2022-07-01 13:36:59 -0400

    Adding simple plugin to update module manifest when building foundry module

--- foundry/vite.config.ts
+++ foundry/vite.config.ts
@@ -1,6 +1,7 @@
+import * as fsPromises from 'fs/promises';
 import copy from 'rollup-plugin-copy';
 import scss from 'rollup-plugin-scss';
-import { defineConfig } from 'vite';
+import { defineConfig, Plugin } from 'vite';
 
 export default defineConfig({
   build: {
@@ -15,6 +16,7 @@ export default defineConfig({
     },
   },
   plugins: [
+    updateModuleManifestPlugin(),
     scss({
       verbose: true,
       output: 'dist/style.css',
@@ -23,7 +25,6 @@ export default defineConfig({
     }),
     copy({
       targets: [
-        { src: 'src/module.json', dest: 'dist' },
         { src: 'src/assets', dest: 'dist' },
         { src: 'src/templates', dest: 'dist' },
       ],
@@ -31,3 +32,32 @@ export default defineConfig({
     }),
   ],
 });
+
+function updateModuleManifestPlugin(): Plugin {
+  return {
+    name: 'update-module-manifest',
+    async writeBundle(options, bundle): Promise<void> {
+      const packageContents = JSON.parse(
+        await fsPromises.readFile('./package.json', 'utf-8'),
+      ) as Record<string, unknown>;
+      const version = packageContents.version as string;
+      const name = packageContents.name as string;
+      const manifestContents: string = await fsPromises.readFile(
+        'src/module.json',
+        'utf-8',
+      );
+      const manifestJson = JSON.parse(manifestContents) as Record<
+        string,
+        unknown
+      >;
+      const baseUrl = 'https://bfdist.us-east-1.linodeobjects.com/foundry';
+      manifestJson['version'] = version;
+      manifestJson['manifest'] = `${baseUrl}/module.json`;
+      manifestJson['download'] = `${baseUrl}/${name}-${version}.zip`;
+      await fsPromises.writeFile(
+        'dist/manifest.json',
+        JSON.stringify(manifestJson),
+      );
+    },
+  };
+}
