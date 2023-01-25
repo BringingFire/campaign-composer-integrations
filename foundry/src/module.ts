06a3fd0e583bbef2fa4a29218fe8f48f943175ee
Joshua Matthews <josh@jmatth.com>
2023-01-25 16:42:45 -0500

    Updating buttons and icons in Foundry module

--- foundry/src/module.ts
+++ foundry/src/module.ts
@@ -31,7 +31,7 @@ Hooks.on('renderJournalDirectory', (_: Application, html: JQuery) => {
 
   const button = $(`<button class="cc-sidebar-button" type="button">
     <img class="cc-sidebar-button-icon" src="modules/${moduleName}/assets/icons/bf-bw.png" title="Campaign Composer"/>
-    <p>Campaign Composer Import</p>
+    Campaign Composer Import
   </button>`);
   button.on('click', () => {
     module.composerBrowser.fetchLists();
