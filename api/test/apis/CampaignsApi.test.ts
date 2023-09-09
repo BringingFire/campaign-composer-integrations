51dbb9b4b713e55974792a97eaf9a14a4069212f
Augmentador <augmentador@gmail.com>
2023-09-08 22:14:25 -0700

    feat: integration tests

---
+++ api/test/apis/CampaignsApi.test.ts
@@ -0,0 +1,131 @@
+import { CampaignsApi, Configuration, DocumentsApi, EntitiesApi, FoldersApi, ImagesApi, LinksApi, MapsApi } from 'campaign-composer-api';
+import { TEST_API_KEY, TEST_CAMPAIGNS_EXPECT_LENGTH, TEST_CAMPAIGN_ID, TEST_DOCUMENTS_EXPECT_LENGTH, TEST_DOCUMENT_ID, TEST_ENTITIES_EXPECT_LENGTH, TEST_ENTITIES_WITH_FOLDER_EXPECT_LENGTH, TEST_ENTITY_ID, TEST_FOLDERS_EXPECT_LENGTH, TEST_FOLDER_ID, TEST_HOST, TEST_IMAGES_EXPECT_LENGTH, TEST_IMAGE_ID, TEST_LINKS_EXPECT_LENGTH, TEST_LINK_ID, TEST_MAPS_EXPECT_LENGTH, TEST_MAP_EXPECT_WIDTH, TEST_MAP_ID } from './data';
+
+const config = new Configuration({
+    basePath: TEST_HOST,
+    apiKey: TEST_API_KEY,
+});
+
+describe('campaigns', () => {
+    const api = new CampaignsApi(config);
+
+    test('list', async () => {
+        const res = await api.listCampaigns();
+        expect(res.length).toBe(TEST_CAMPAIGNS_EXPECT_LENGTH);
+        const campaign = res[0];
+        expect(campaign.id).toBe(TEST_CAMPAIGN_ID);
+    });
+});
+
+describe('documents', () => {
+    const api = new DocumentsApi(config);
+
+    test('list', async () => {
+        const res = await api.listDocuments({ campaignId: TEST_CAMPAIGN_ID });
+        expect(res.length).toBe(TEST_DOCUMENTS_EXPECT_LENGTH);
+        const doc = res[0];
+        expect(doc.id).toBe(TEST_DOCUMENT_ID);
+    });
+
+    test('get', async () => {
+        const res = await api.getDocument({ campaignId: TEST_CAMPAIGN_ID, docId: TEST_DOCUMENT_ID });
+        expect(res.id).toBe(TEST_DOCUMENT_ID);
+    });
+});
+
+describe('maps', () => {
+    const api = new MapsApi(config);
+
+    test('list', async () => {
+        const res = await api.listMaps({ campaignId: TEST_CAMPAIGN_ID });
+        expect(res.length).toBe(TEST_MAPS_EXPECT_LENGTH);
+        const map = res[0];
+        expect(map.id).toBe(TEST_MAP_ID);
+    });
+
+    test('get', async () => {
+        const res = await api.getMap({ campaignId: TEST_CAMPAIGN_ID, mapId: TEST_MAP_ID });
+        expect(res.id).toBe(TEST_MAP_ID);
+    });
+
+    test('get map background', async () => {
+        const res = await api.getMapBackground({ campaignId: TEST_CAMPAIGN_ID, mapId: TEST_MAP_ID });
+        expect(res.width).toBe(TEST_MAP_EXPECT_WIDTH);
+    });
+});
+
+describe('folders', () => {
+    const api = new FoldersApi(config);
+
+    test('list', async () => {
+        const res = await api.listFolders({ campaignId: TEST_CAMPAIGN_ID });
+        expect(res.length).toBe(TEST_FOLDERS_EXPECT_LENGTH);
+        const folder = res[0];
+        expect(folder.id).toBe(TEST_FOLDER_ID);
+    });
+
+    test('get', async () => {
+        const res = await api.getFolder({ campaignId: TEST_CAMPAIGN_ID, folderId: TEST_FOLDER_ID });
+        expect(res.id).toBe(TEST_FOLDER_ID);
+    });
+});
+
+
+describe('links', () => {
+    const api = new LinksApi(config);
+
+    test('list', async () => {
+        const res = await api.listLinks({ campaignId: TEST_CAMPAIGN_ID });
+        expect(res.length).toBe(TEST_LINKS_EXPECT_LENGTH);
+        const link = res[0];
+        expect(link.id).toBe(TEST_LINK_ID);
+    });
+
+    test('get', async () => {
+        const res = await api.getLink({ campaignId: TEST_CAMPAIGN_ID, linkId: TEST_LINK_ID });
+        expect(res.id).toBe(TEST_LINK_ID);
+    });
+});
+
+describe('images', () => {
+    const api = new ImagesApi(config);
+
+    test('list', async () => {
+        const res = await api.listImages({ campaignId: TEST_CAMPAIGN_ID });
+        expect(res.length).toBe(TEST_IMAGES_EXPECT_LENGTH);
+        const image = res[0];
+        expect(image.id).toBe(TEST_IMAGE_ID);
+    });
+
+    test('get', async () => {
+        const res = await api.getImage({ campaignId: TEST_CAMPAIGN_ID, imageId: TEST_IMAGE_ID });
+        expect(res.id).toBe(TEST_IMAGE_ID);
+    });
+});
+
+describe('entities', () => {
+    const api = new EntitiesApi(config);
+
+    describe('list', () => {
+        test('without parameters', async () => {
+            const res = await api.listEntities({ campaignId: TEST_CAMPAIGN_ID });
+            expect(res.length).toBe(TEST_ENTITIES_EXPECT_LENGTH);
+        });
+
+        test('with parameters', async () => {
+            const res = await api.listEntities({
+                campaignId: TEST_CAMPAIGN_ID,
+                folderId: TEST_FOLDER_ID,
+            });
+            expect(res.length).toBe(TEST_ENTITIES_WITH_FOLDER_EXPECT_LENGTH);
+        });
+    });
+
+    test('get entity', async () => {
+        const res = await api.getEntity({
+            campaignId: TEST_CAMPAIGN_ID,
+            entityId: TEST_ENTITY_ID
+        });
+        expect(res.id).toBe(TEST_ENTITY_ID);
+    });
+});
---
+++ api/test/apis/data.ts
@@ -0,0 +1,25 @@
+export const TEST_HOST = 'http://localhost:4492';
+export const TEST_API_KEY = 'tDeQrn9K0IilKsoiXKM8wqBi';
+
+export const TEST_CAMPAIGNS_EXPECT_LENGTH: number = 1;
+export const TEST_CAMPAIGN_ID: number = 1;
+
+export const TEST_DOCUMENTS_EXPECT_LENGTH: number = 5;
+export const TEST_DOCUMENT_ID: string = 'a3cc0568-f013-4b23-a0de-ba64b2c54f8a';
+
+export const TEST_MAPS_EXPECT_LENGTH: number = 2;
+export const TEST_MAP_ID: string = '1e647a6e-fefa-4d38-a98e-d0ef325087bd';
+export const TEST_MAP_EXPECT_WIDTH: number = 4350;
+
+export const TEST_FOLDERS_EXPECT_LENGTH: number = 3;
+export const TEST_FOLDER_ID: number = 1;
+
+export const TEST_LINKS_EXPECT_LENGTH: number = 1;
+export const TEST_LINK_ID: string = '0a38c8a5-3b68-4167-890c-2ce597d9f0d5';
+
+export const TEST_IMAGES_EXPECT_LENGTH: number = 1;
+export const TEST_IMAGE_ID: string = '09141dc6-9e4d-4430-a913-db6ffaccfa4a';
+
+export const TEST_ENTITIES_EXPECT_LENGTH: number = 7;
+export const TEST_ENTITIES_WITH_FOLDER_EXPECT_LENGTH: number = 1;
+export const TEST_ENTITY_ID: number = 1;
