ac087ce0cb0cc5f1a7905c7f0059868ec4d3faf2
Joshua Matthews <josh@jmatth.com>
2023-09-12 11:02:14 -0400

    Fixing link type, improving related test

--- api/test/apis/data.ts
+++ api/test/apis/data.ts
@@ -13,6 +13,7 @@ export const TEST_FOLDER_ID: number = 1;
 
 export const TEST_LINKS_EXPECT_LENGTH: number = 1;
 export const TEST_LINK_ID: string = '0a38c8a5-3b68-4167-890c-2ce597d9f0d5';
+export const TEST_LINK_URL: string = 'https://bringingfire.com';
 
 export const TEST_IMAGES_EXPECT_LENGTH: number = 1;
 export const TEST_IMAGE_ID: string = '09141dc6-9e4d-4430-a913-db6ffaccfa4a';
